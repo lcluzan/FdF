73dbbc6ee6ee9e3d9a1948552e02eaabf1887c00
lcluzan <lcluzan@student.42.fr>
2024-10-24 17:44:56 +0200

    add comments

--- srcs/map.c
+++ srcs/map.c
@@ -6,12 +6,15 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:23:26 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/10/21 12:23:27 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/10/24 17:44:43 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdlib.h>
+
+//Safely allocate a map struct and initialize some fields.
+// allocate and init map
 
 t_map	*new_map(void)
 {
@@ -25,6 +28,8 @@ t_map	*new_map(void)
 	map->lines = NULL;
 	return (map);
 }
+
+//Free a map and all its content without memory leaks.
 
 void	free_map(t_map *map)
 {
