5d770aa373e954c79b9cb30cc6963b598e1c118c
lcluzan <lcluzan@student.42.fr>
2024-10-24 18:21:32 +0200

    add comments

--- srcs/math_utils.c
+++ srcs/math_utils.c
@@ -6,12 +6,13 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:23:37 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/10/21 12:23:38 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/10/24 18:21:16 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+//return the absolute value.
 int32_t	abs(int32_t n)
 {
 	if (n < 0)
@@ -20,6 +21,7 @@ int32_t	abs(int32_t n)
 		return (n);
 }
 
+//return the smallest value between 2 parameters.
 int32_t	min(int32_t a, int32_t b)
 {
 	if (a < b)
@@ -28,6 +30,7 @@ int32_t	min(int32_t a, int32_t b)
 		return (b);
 }
 
+//return the largest value between 2 parameters.
 int32_t	max(int32_t a, int32_t b)
 {
 	if (a > b)
@@ -35,7 +38,7 @@ int32_t	max(int32_t a, int32_t b)
 	else
 		return (b);
 }
-
+//compute the distance between 2 signed values (eg. diff(-2, 1) = 3)
 int32_t	diff(int32_t a, int32_t b)
 {
 	if ((a < 0 && b < 0) || (a >= 0 && b >= 0))
