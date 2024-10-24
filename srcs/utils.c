8da5c0f30a8db38fe02da1396f15f1db75a05c0b
lcluzan <lcluzan@student.42.fr>
2024-10-24 18:21:43 +0200

    add comments

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,21 +6,21 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:24:05 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/10/21 12:24:06 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/10/24 18:18:19 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdbool.h>
 #include "fdf.h"
-#include <math.h>
 
+//abort the program with an error message.
 void	exit_with_error(const char *message)
 {
 	perror(message);
 	exit(-1);
 }
 
+//check that a character is alphabetic and valid in an hex string (basically
+// if it's either a, b, c, d, e, or f).
 bool	is_hex_alpha(int c)
 {
 	if (c >= 'a' && c <= 'f')
@@ -29,6 +29,15 @@ bool	is_hex_alpha(int c)
 		return (false);
 }
 
+/*
+  This function will loop through each point of a map, and call the function
+  passed in parameters `fn`.
+  Passing to this function a pointer on the point, and the paramter `params`.
+
+  This way we can apply some specific changes to every points of the map without
+   having to write the same looping logic
+  every time.
+*/
 void	for_each_point(
 	t_map *map,
 	void (*fn)(t_point *point, void *params),
@@ -51,6 +60,7 @@ void	for_each_point(
 	}
 }
 
+//round a point coords
 t_point	round_point(t_point p)
 {
 	p.v.x = round(p.v.x);
@@ -59,6 +69,7 @@ t_point	round_point(t_point p)
 	return (p);
 }
 
+//Just open a file and make sure it worked.
 int	open_map(char *map_path)
 {
 	int	fd;
