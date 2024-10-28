8e8914d748b44eb7a6b67811dcbc7c2f21011905
lcluzan <lcluzan@student.42.fr>
2024-10-28 16:04:33 +0100

    remove ft_strjoin

--- libft/get_next_line_utils.c
+++ libft/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 11:31:36 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/07/17 16:47:22 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/10/28 14:47:24 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,27 +59,6 @@ char	*ft_substr(char const *s, size_t start, size_t len)
 		i++;
 	}
 	new[i] = '\0';
-	return (new);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		i;
-	int		j;
-	char	*new;
-
-	i = 0;
-	j = 0;
-	new = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!new)
-		return (NULL);
-	while (s1[i])
-		new[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		new[j++] = s2[i++];
-	new[j] = 0;
-	free ((void *)(s1));
 	return (new);
 }
 
